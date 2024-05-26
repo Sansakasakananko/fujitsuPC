9a2f8c305318c052a49bedb592c13ad0f7d72c2a
sansakasakananoko <misaka.seiko@gmail.com>
2024-05-26 15:05:43 +0900

    first refactaling

--- bowling/spec/lib/bowling_spec.rb
+++ bowling/spec/lib/bowling_spec.rb
@@ -1,31 +1,39 @@
+# bowling_spec.rb
+
 require "bowling"
 
-describe "ボーリングのスコア計算" do
-    describe "全体の計算" do
-        context "すべての投球がガターになること" do
-            it "0になること" do
-                @game = Bowling.new
 
-                20.times do
-                    @game.add_score(0)
-                end
-
-                expect(@game.add_scpre).to eq 0
-            end
-        end
-
-        context "すべての投球で1ピンずつ倒した場合" do
-            it "20になること" do
-                @game = Bowling.new
-
-                20.times do
-                    @game.add_score(20)
-                end
-
-                expect(@game.add_score).to eq 20
-            end
-        end
+describe "ボウリングのスコア計算" do
+    #インスタンスの生成を共通化
+    before do
+        @game = Bowling.new
     end
+
+
+  describe "全体の合計" do
+    context "全ての投球がガターだった場合" do
+      it "0になること" do
+        add_many_scores(20, 0)
+        expect(@game.total_score).to eq 0
+      end
+    end
+
+    context "全ての投球で1ピンずつ倒した場合" do
+      it "20になること" do
+        add_many_scores(20, 1)
+        expect(@game.total_score).to eq 20
+      end
+    end
+
+
+  end
 end
 
 
+private 
+#複数回のスコア追加をまとめて実行する
+def add_many_scores(count, pins)
+    count.times do
+        @game.add_score(pins)
+    end
+end
